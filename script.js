34fe0d5259d595801f3c56329e369d8761a292d8
Andrei Larroque <andrei.larroque@hotmail.com>
2022-02-23 11:40:56 -0300

    versao 5

--- script.js
+++ script.js
@@ -10,4 +10,24 @@ $(document).ready(function(){
 	$('.menu-btn').click(function(){
 		$('.navbar .menu').toggleClass("active");
 	});
+	$('.carousel').owlCarousel({
+		margin:20,
+		loop:true,
+		autoplayTimeOut:2000,
+		autplayHoverPauser:true,
+		responsive:{
+			0:{
+				items:1,
+				nav:false
+			},
+			600:{
+				items:2,
+				nav:false
+			},
+			1000:{
+				items:3,
+				nav:false
+			}
+		}
+	})
 });
