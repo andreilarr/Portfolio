fa3ffc7c37d0e344e95655ceecc05c39cbaa2220
Andrei Larroque <andrei.larroque@hotmail.com>
2022-02-23 03:37:45 -0300

    versao 2

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ $(document).ready(function(){
 		}else{
 			$('.navbar').removeClass("sticky");
 		}
-	})
+	});
+	//toggle menu/navbar script
+	$('.menu-btn').click(function(){
+		$('.navbar .menu').toggleClass("active");
+	});
 });
